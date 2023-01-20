d151ba9cc00623e29fd9d1004784ef806406cc94
Andrea Hergert <ahergert24@hotmail.com>
2023-01-20 13:00:41 -0800

    added const for typedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 // const routes = require('./routes');
 
